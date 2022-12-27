d4a5f2e25575ebc37b2789292de7e8753a1ff2a0
IddoSadeh <44779994+IddoSadeh@users.noreply.github.com>
2022-12-26 20:12:17 -0800

    added S1 2022 comments

--- 2022/s1.py
+++ 2022/s1.py
@@ -4,11 +4,14 @@ N =  int(input())
 #N = 4x+5y
 # how many x,y combinations are there?
 
-#we can brute force it
+#we can brute force it for small numbers. create a list with the solutions for different numbers. 
+#This will get you the first three points of the excercise but nothing more
 # 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 #[0,0,0,0,1,1,0,0,1,0,1,0,1,0,1,1,1,1,0,1,2]
 
 # we can nest loops
+# we have two for loops for a time comlpexity of O((N/4)(N/5)) = O(N^2/20)
+# not efficient, notice you will timeout for large inputs
 start = time.time()
 count = 0
 for i in range(N//4+1):
@@ -20,6 +23,9 @@ end = time.time()
 print(end-start)
 
 # A bit smarter, only loop through x, and see if y is divisble by 5
+# this will get you the majority of the points
+# complexity of O(N/4)
+# try tracing this solution with small inputs and compare to previous solution
 start = time.time()
 count = 0
 for i in range(N//4+1):
@@ -29,7 +35,9 @@ print(count)
 end = time.time()
 print(end-start)
 
-#fastest solution:
+# fastest solution:
+# pretty much magic
+# take some time to appreciate this one
 start = time.time()
 first20 = [0,0,0,0,1,1,0,0,1,0,1,0,1,0,1,1,1,1,0,1,2]
 count = 0
